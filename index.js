ec6d5101602dee50302db21edc7be6af42ae7899
Dee <dgrowette@gmail.com>
2023-08-06 19:48:12 -0700

    fizz buzz practice

---
+++ index.js
@@ -0,0 +1,12 @@
+let num = 0
+
+if (num % 3 === 0) {
+    console.log('fizz')
+}else if (num % 5 === 0) {
+    console.log('buzz')
+}else if (num % 15 === 0) {
+    console.log('fizz buzz')
+} else {
+    console.log(num)
+}
+
