91be2fdee9dd8e1b6ce1b6255ca1b91745495d59
Dee <dgrowette@gmail.com>
2023-08-06 19:56:32 -0700

    for loop with fizz buzz

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 let num = 0
 
-if (num % 3 === 0) {
-    console.log('fizz')
-}else if (num % 5 === 0) {
-    console.log('buzz')
-}else if (num % 15 === 0) {
-    console.log('fizz buzz')
-} else {
-    console.log(num)
+for (num=1; num <101; num++){
+    if ((num % 3) === 0) {
+        console.log('fizz')
+    }else if ((num % 5) === 0) {
+        console.log('buzz')
+    }else if ((num % 15) === 0) {
+        console.log('fizz buzz')
+    } else {
+        console.log(num+1)
+    }
+
 }
+
 
