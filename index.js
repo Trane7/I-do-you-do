6b219a9999a4bbb4e70b67e9e53be6ffd03306f8
Dee <dgrowette@gmail.com>
2023-08-08 20:03:14 -0700

    deleted the fizz buzz code

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-let num = 0
-
-for (num=1; num <101; num++){
-    if ((num % 3) === 0) {
-        console.log('fizz')
-    }else if ((num % 5) === 0) {
-        console.log('buzz')
-    }else if ((num % 15) === 0) {
-        console.log('fizz buzz')
-    } else {
-        console.log(num+1)
-    }
-
-}
 
 
