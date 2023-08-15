8c1ff5f34262a1ccfac27add578c677da992c57e
Dee <dgrowette@gmail.com>
2023-08-14 21:44:19 -0700

    added db.js file to server folder

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const PORT = process.env.PORT  ?? 8000
+const express = require('express')
+const app = express()
+
+// get all youdos
+app.get('/youdos', async (req, res) => {
+
+    try {
+        // await 
+    } catch (err) {
+        console.log(error)
+    }
+})
+
+
+
+app.listen(PORT, ( )=> console.log(`Server running on PORT ${PORT}`))
