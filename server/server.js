eb638a672d69db71a040135525a0fc820522e901
Trane7 <dgrowette@gmail.com>
2023-08-21 12:46:10 -0700

    added /:userEmail call to server.js file

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const PORT = process.env.PORT  ?? 8000
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const pool = require('./db')
 
+
+app.use(cors())
+
 // get all youdos
-app.get('/youdos', async (req, res) => {
+app.get('/youdos/:userEmail/', async (req, res) => {
     console.log(req)
     //test email for array
-    const userEmail = req.params
+    const { userEmail } = req.params
     //this is testing for the data
     try {
        const youdos = await pool.query('SELECT * FROM youdos WHERE user_email = $1', [userEmail])
