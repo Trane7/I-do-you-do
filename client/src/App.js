7aab8fdaa4f16d18a104bfb0d8ead06f3d1c2392
Dee <dgrowette@gmail.com>
2023-08-22 19:19:36 -0700

    imported ListItem into the app.js for tasks

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import ListHeader from './components/ListHeader'
-import { useEffect } from 'react'
+import ListItem from './components/ListItem'
+import { useEffect, useState } from 'react'
+
 
 
 const App = () => {
+  const userEmail = 'trane@test.com'
+  const [ tasks, setTasks] = useState(null)
 
 const getData = async ()=> {
-  const userEmail = 'trane@test.com'
   try {
     //passing a param to find the user's email
     const response = await fetch(`http://localhost:8000/youdos/${userEmail}`)
     const json = await response.json()
     console.log(json)
+    setTasks(json)
   } catch (err) {
     console.error(err)
   }
@@ -18,11 +22,18 @@ const getData = async ()=> {
 
  useEffect(() => getData, [])
 
+ // logs the tasks in the console
+ console.log(tasks)
+
+ // This will be sorting by the date
+ const sortedTasks = tasks?.sort((a,b) => new Date(a.date) - new Date(b.date))
+
 
 
   return (
     <div className="app"> 
       <ListHeader listName={'Holiday tick list'}/>
+      {sortedTasks?.map((tasks) => <ListItem key={tasks.id} task={task} />)}
     </div>
   );
 }
