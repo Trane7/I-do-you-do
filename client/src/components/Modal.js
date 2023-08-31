e6e443564880f5e219e31c508732750d3898028a
Trane7 <dgrowette@gmail.com>
2023-08-31 12:01:47 -0700

    started my modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,6 +1,32 @@
 const Modal = () => {
+
+    const mode = 'edit'
+
+    const handleChange = () => {
+      console.log('changing!')
+    }
     return (
-      <div> 
+      <div className="overlay">
+        <div className="modal">
+          <div className="form-title-container">
+            <h3>Lets {mode} your task</h3>
+            <button>X</button>
+          </div>
+
+          <form>
+            <input
+            required
+            maxLength={30}
+            placeHolder=" Your task goes here"
+            name="title"
+            value={""}
+            onChange={handleChange}
+            />
+            <input/>
+            <input type="submit"/>
+          </form>
+
+        </div> 
       </div>
     );
   }
