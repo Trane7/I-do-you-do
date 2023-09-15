f3303d25a7d6b802e18b483442066d525214d549
Trane7 <dgrowette@gmail.com>
2023-09-15 12:42:47 -0700

    started my login for auth.js file

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,6 +1,20 @@
 const Auth = () => {
+
+  const isLogIn = false
+
+
     return (
-      <div> 
+      <div className="auth-container">
+        <div className="auth-container-box">
+          <form>
+            <h2>{isLogIn ? 'Please log in' : 'Please sign up'}</h2>
+            <input type="email" placeholder="email"/>
+            <input type="password" placeholder="password" />
+            <input type="password" placeholder="confirm password"/>
+            
+          </form>
+          
+        </div> 
       </div>
     );
   }
